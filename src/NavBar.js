805ff1a9acc77c07c94af1cf6baff0e496da5b39
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-20 15:17:11 +0000

    updated NavBar component and made it responsive

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,15 +3,28 @@ import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <div className="topnav">
-      <a href="#name" className="name">
-        RB
-      </a>
-      <div className="links">
-        <a href="#home">Home</a>
-        <a href="#project">Projects</a>
-        <a href="#contact">Contact</a>
+    <nav className="navbar">
+      <div className="logo">RB</div>
+      <div className="nav-links">
+        <a href="/" className="nav-link">
+          Home
+        </a>
+        <div className="dropdown">
+          <a href="#" className="nav-link">
+            Projects
+          </a>
+          <div className="dropdown-content">
+            <a href="#">Dictionary App</a>
+            <a href="#">Weather App</a>
+            <a href="#">Stephen Sanchez</a>
+            <a href="#">Bratz</a>
+            <a href="#">Quotes Generator</a>
+          </div>
+        </div>
+        <a href="/contact" className="nav-link">
+          Contact
+        </a>
       </div>
-    </div>
+    </nav>
   );
 }
