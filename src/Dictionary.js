608d27d75c648deb28afab6066fd73cb0cfa25b5
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-24 14:10:05 +0000

    moved icon components into their own folder

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,48 +1,45 @@
 import React from "react";
-import HTMLIcon from "./HTMLIcon";
-import CSSIcon from "./CSSIcon";
-import JSIcon from "./JSIcon";
-import VSCodeIcon from "./VSCodeIcon";
-import APIIcon from "./APIIcon.js";
-import GitIcon from "./GitIcon.js";
-import BootstrapIcon from "./BootstrapIcon.js";
-import HostingIcon from "./HostingIcon.js";
-import ReactIcon from "./ReactIcon.js";
+
+import CSSIcon from ".//IconComponents/CSSIcon";
+import JSIcon from ".//IconComponents/JSIcon";
+import VSCodeIcon from ".//IconComponents/VSCodeIcon";
+import APIIcon from ".//IconComponents/APIIcon.js";
+import GitIcon from ".//IconComponents/GitIcon.js";
+import BootstrapIcon from ".//IconComponents/BootstrapIcon.js";
+import HostingIcon from ".//IconComponents/HostingIcon.js";
+import ReactIcon from ".//IconComponents/ReactIcon.js";
 import "./Dictionary.css";
+
+const icons = [
+  { component: <CSSIcon />, name: "CSS" },
+  { component: <JSIcon />, name: "JavaScript" },
+  { component: <VSCodeIcon />, name: "VSCode" },
+  { component: <APIIcon />, name: "API" },
+  { component: <GitIcon />, name: "Git" },
+  { component: <BootstrapIcon />, name: "Bootstrap" },
+  { component: <HostingIcon />, name: "Hosting" },
+  { component: <ReactIcon />, name: "React" },
+];
 
 export default function Dictionary() {
   return (
     <div>
       <h2>Dictionary app</h2>
       <p>Languages used in this project:</p>
-      <div className="dictionary-container">
-        <div className="icon-container">
-          <HTMLIcon />
-        </div>
-        <div className="icon-container">
-          <CSSIcon />
-        </div>
-        <div className="icon-container">
-          <JSIcon />
-        </div>
-        <div className="icon-container">
-          <VSCodeIcon />
-        </div>
-        <div className="icon-container">
-          <APIIcon />
-        </div>
-        <div className="icon-container">
-          <GitIcon />
-        </div>
-        <div className="icon-container">
-          <BootstrapIcon />
-        </div>
-        <div>
-          <HostingIcon />
-        </div>
-        <div>
-          <ReactIcon />
-        </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "20px",
+          fontsize: "12px",
+        }}
+      >
+        {icons.map((icon, index) => (
+          <div key={index} style={{ textAlign: "center" }}>
+            {icon.component}
+            <p>{icon.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
