a0b8b0f5225edf4386552c07ff79e8692f0c35f9
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-25 16:16:48 +0000

    included grids and embedded site

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import CSSIcon from ".//IconComponents/CSSIcon";
 import JSIcon from ".//IconComponents/JSIcon";
 import VSCodeIcon from ".//IconComponents/VSCodeIcon";
@@ -23,67 +22,76 @@ const icons = [
 
 export default function Dictionary() {
   return (
-    <div>
-      <h2
-        style={{
-          marginBottom: "5px",
-        }}
-      >
-        Dictionary app
-      </h2>
-      <p
-        style={{
-          marginLeft: "20px",
-        }}
-      >
-        Languages used in this project:
-      </p>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(4, 1fr)",
-          gap: "20px",
-          fontsize: "12px",
-        }}
-      >
-        {icons.map((icon, index) => (
-          <div key={index} style={{ textAlign: "center" }}>
-            {icon.component}
-            <p>{icon.name}</p>
-          </div>
-        ))}
-      </div>
-      <div className="container">
-        <div className="content">
-          <p className="dictionary-para">
+    <div style={{ display: "grid", gridTemplateColumns: "50% 50%" }}>
+      <div style={{ width: "100%" }}>
+        <h2
+          style={{
+            marginBottom: "5px",
+          }}
+        >
+          Dictionary app
+        </h2>
+        <p className="language-para">Languages used in this project:</p>
+        <div
+          style={{
+            maxWidth: "900px",
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: "20px",
+            fontsize: "12px",
+          }}
+        >
+          {icons.map((icon, index) => (
+            <div key={index} style={{ textAlign: "center" }}>
+              {icon.component}
+              <p>{icon.name}</p>
+            </div>
+          ))}
+        </div>
+        <div className="grid-container">
+          <div className="dictionary-para">
             This project was dedicated to developing Advanced React Development
             Skills. The task was to build a dictionary app using two APIs, that
             would allow the user to receive information based on their search.
             This project allowed for a more advanced knowledge of React and
             built up my confidence to create a more seamless project.
-          </p>
+          </div>
+
+          <div>
+            <button className="git-button">
+              <a
+                href="https://github.com/rae8108/dictionary-project"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </button>
+          </div>
+          <div>
+            <button className="web-button">
+              <a
+                href="https://dictionary-project-sun.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Website
+              </a>
+            </button>
+          </div>
         </div>
-        <div className="button-container">
-          <button className="icon-button">
-            <a
-              href="https://github.com/rae8108/dictionary-project"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </button>
-          <button className="icon-button">
-            <a
-              href="https://dictionary-project-sun.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Website
-            </a>
-          </button>
-        </div>
+      </div>
+      <div
+        style={{
+          marginTop: "10px",
+        }}
+      >
+        <iframe
+          src="https://dictionary-project-sun.netlify.app/"
+          title="Embedded Website"
+          width="800"
+          height="500"
+        ></iframe>
       </div>
     </div>
   );
