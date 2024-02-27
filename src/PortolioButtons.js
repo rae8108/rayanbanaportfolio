e2d331ff04e813463a03c976ed682622ee7d84a9
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-27 17:14:25 +0000

    moved site links to the buttons

--- src/PortolioButtons.js
+++ src/PortolioButtons.js
@@ -4,11 +4,51 @@ import "./PortfolioButtons.css";
 export default function PortfolioButtons() {
   return (
     <div className="Portfolio-Buttons">
-      <button>Dictionary App </button>
-      <button>World Clock </button>
-      <button>Stephen Sanchez </button>
-      <button>Bratz </button>
-      <button>Quotes Generator </button>
+      <button>
+        <a
+          href="https://dictionary-project-sun.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dictionary App
+        </a>
+      </button>
+      <button>
+        <a
+          href="https://world-clock-rae.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          World Clock
+        </a>
+      </button>
+      <button>
+        <a
+          href="https://sanchez-responsive.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Stephen Sanchez
+        </a>
+      </button>
+      <button>
+        <a
+          href="https://silver-kitten-304eaf.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Bratz
+        </a>
+      </button>
+      <button>
+        <a
+          href="https://positive-quotes-generator.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Quotes Generator
+        </a>
+      </button>
     </div>
   );
 }
