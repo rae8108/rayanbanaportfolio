177a603838f5d74004251650f3d921dca425389a
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-27 17:13:21 +0000

    updated component

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,9 @@ export default function home() {
   return (
     <div className="Home">
       <h2>Portfolio</h2>
+      <p className="link-button">
+        Click the buttons below to be taken to the website
+      </p>
     </div>
   );
 }
