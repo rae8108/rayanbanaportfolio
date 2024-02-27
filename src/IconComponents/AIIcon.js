dbeee6c41d09759b1be924a4332933d5a2134a3b
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-27 17:13:39 +0000

    created AI Icon component

---
+++ src/IconComponents/AIIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ai from "./ai.jpeg";
+
+export default function AIIcon() {
+  return (
+    <div>
+      <img src={ai} alt="ai-icon" className="ai-icon" />
+    </div>
+  );
+}
